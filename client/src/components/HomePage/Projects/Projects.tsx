e79773386d4e56bdef93ad830b4236c2dcfb55b8
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-07-25 14:55:40 +0300

    adding info to projects component to small screens

--- client/src/components/HomePage/Projects/Projects.tsx
+++ client/src/components/HomePage/Projects/Projects.tsx
@@ -1,12 +1,13 @@
 import Title from "../../Title/Title";
 import "../style.scss";
 import ProjectCard from "./ProjectCard/ProjectCard";
-
+import './style.scss';
 
 const Projects = () => {
   return (
     <div id="projects" className="same-container">
       <Title title={"PROJECTS"} />
+      <p className="para-on-sm-screens">Scroll sideways to see more projects</p>
       <ProjectCard />
     </div>
   );
